6526199d2788fb7a9ac7f46af3c23667de67841b
Alexander Bryantsev <a.i.bryantsev@gmail.com>
2019-06-30 13:48:41 +0300

    Рефакторинг для повышения читабельности кода

--- hw01-maven/src/main/java/ru/otus/lesson1/HelloOtus.java
+++ hw01-maven/src/main/java/ru/otus/lesson1/HelloOtus.java
@@ -11,14 +11,34 @@ public class HelloOtus {
 
     public static void main(String[] args) {
 
-        Console console = System.console();
+        Console console = getConsole();
         if (console == null) {
-            System.out.println("Консоль недоступна! Запустите приложение из терминала или командной строки!\n");
             return;
         }
 
+        String userName = readUserName(console);
+        if (!checkUserPassword(console, userName, new String(console.readPassword("Пароль: ")))) {
+            return;
+        }
+
+        // Useful work next
+    }
+
+    private static boolean checkUserPassword(Console console, String userName, String password) {
         Map<String, String> users = ImmutableMap.of("user1", "pass1", "user2", "pass2");
 
+        if (!Strings.isNullOrEmpty(password) &&
+                users.containsKey(userName) &&
+                users.getOrDefault(userName, "").equals(password)) {
+            console.printf("Добро пожаловать, %s!", userName);
+            return true;
+        } else {
+            console.printf("Неверное имя пользователя или пароль!");
+            return false;
+        }
+    }
+
+    private static String readUserName(Console console) {
         String userName;
         do {
             userName = console.readLine("Имя пользователя: ");
@@ -29,15 +49,15 @@ public class HelloOtus {
             }
         } while (true);
 
-        String password = new String(console.readPassword("Пароль: "));
-        if (!Strings.isNullOrEmpty(password) &&
-                users.containsKey(userName) &&
-                users.getOrDefault(userName, "").equals(password)) {
-            console.printf("Добро пожаловать, %s!", userName);
-        } else {
-            console.printf("Неверное имя пользователя или пароль!");
-        }
+        return userName;
+    }
 
+    private static Console getConsole() {
+        Console console = System.console();
+        if (console == null) {
+            System.out.println("Консоль недоступна! Запустите приложение из терминала или командной строки!\n");
+        }
+        return console;
     }
 
 }
